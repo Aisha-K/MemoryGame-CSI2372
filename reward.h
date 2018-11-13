a8df0e67a617cf9513c16989c106e8b4f224dc3d
Aisha Khalid <akhal096@uottawa.ca>
2018-11-13 10:13:12 -0500

    Implemendted friend access

--- reward.h
+++ reward.h
@@ -4,11 +4,14 @@
 #ifndef REWARD_H
 #define REWARD_H
 
+
 class Reward{
     int rubyValue;
 
-    //Reward::Deck(int rubyValue);
-    Reward(int rubyValue);
+    //gives friend access to the function Reward* Reward(int rubyValue) in class rewardDeck, which will create Reward objects
+    friend Reward* RewardDeck::Reward(int rubyValue);
+
+    Reward(int rubyVal): rubyValue(rubyVal){};  //constructor
     friend std::ostream& operator<<(std:: ostream& stream, const Reward& reward);
 
 };
--- rewardDeck.h
+++ rewardDeck.h
@@ -1,9 +1,21 @@
 #include "cardDeck.h"
 
+
 #ifndef REWARDDECK_H
 #define REWARDDECK_H
 
+class Reward;
+
 class RewardDeck{
+    Reward *rewardPtr;
+    //Reward reward;
+
+public:
+    RewardDeck();
+
+    //has frienc access to reward, will create objects of type Reward
+    Reward* Reward(int rubyValue);
+
 
 
 
